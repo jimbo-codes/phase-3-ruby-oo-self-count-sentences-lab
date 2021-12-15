89949789fedc710f0fc3f7675b58e385b55f4a39
James Burns <jrburns96@aol.com>
2021-12-15 11:31:22 -0500

    commit tree 6a1d818fbb0a98d6191493b9a085bbc163a88a93

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?"."
   end
 
   def question?
-
+    self.include?"?"
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    sentence_arr = self.split(/[.!?]+/)
+    sentence_arr.length    
   end
 end
+"Here we have some sentences. Then we have More.".count_sentences
